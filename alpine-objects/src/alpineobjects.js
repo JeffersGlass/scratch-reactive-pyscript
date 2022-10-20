60e4e56fec91f0bf5bc70d1b82327becde5015a6
Jeff Glass <glass.jeffrey@gmail.com>
2022-10-19 22:15:15 -0500

    Working automatic demo using settracfe

--- alpine-objects/src/alpineobjects.js
+++ alpine-objects/src/alpineobjects.js
@@ -2,11 +2,12 @@ import Alpine from 'alpinejs'
 
 window.Alpine = Alpine
 
+console.warn("Created pythonGlobals store")
+
 document.addEventListener('pyscriptReady', () => {
     console.warn('Pyscript has Started')
     Alpine.store('pythonGlobals', {
         globals: pyscript.runtime.globals, 
-        id: 0,
         refresh() {
             //Must set the object to point to something else, then back to globals, to get it to refresh
             this.globals = {}
@@ -14,9 +15,12 @@ document.addEventListener('pyscriptReady', () => {
         }})
     Alpine.start()
     console.warn('Alpine has started')
+
+    pyscript.runtime.globals.get('setRefresh')()
 })
 
 document.getElementById('btn-store').addEventListener("click", () => {
     console.log(pyscript.runtime.globals.toJs())
     Alpine.store('pythonGlobals').refresh()
 })
+
--- alpine-objects/src/alpineobjects.py
+++ alpine-objects/src/alpineobjects.py
@@ -1,6 +1,13 @@
-from js import console, document, Event
+from js import console, document, Event, Alpine
+import sys
 console.log("PyScript loaded")
 
-pyscriptReady = Event.new('pyscriptReady')
+def setRefresh():
+    console.log(dir(sys._getframe()))
+    sys._getframe().f_trace = Alpine.store('pythonGlobals').refresh
+    sys.settrace(Alpine.store('pythonGlobals').refresh)
+    console.warn("System Traces set in python")
 
+console.log("Sending pyscriptReady event")
+pyscriptReady = Event.new('pyscriptReady')
 document.dispatchEvent(pyscriptReady);
