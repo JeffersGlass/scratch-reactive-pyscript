ca01392e292bc4be36a00b404eb0dad2eeaf3512
Jeff Glass <glass.jeffrey@gmail.com>
2022-10-19 21:57:29 -0500

    Working demo with pyscript.runtime.globals, still need to manually refresh things

--- alpine-objects/src/alpineobjects.js
+++ alpine-objects/src/alpineobjects.js
@@ -1,1 +1,22 @@
-console.log("JS Loaded")
+import Alpine from 'alpinejs'
+
+window.Alpine = Alpine
+
+document.addEventListener('pyscriptReady', () => {
+    console.warn('Pyscript has Started')
+    Alpine.store('pythonGlobals', {
+        globals: pyscript.runtime.globals, 
+        id: 0,
+        refresh() {
+            //Must set the object to point to something else, then back to globals, to get it to refresh
+            this.globals = {}
+            this.globals = pyscript.runtime.globals
+        }})
+    Alpine.start()
+    console.warn('Alpine has started')
+})
+
+document.getElementById('btn-store').addEventListener("click", () => {
+    console.log(pyscript.runtime.globals.toJs())
+    Alpine.store('pythonGlobals').refresh()
+})
--- alpine-objects/src/alpineobjects.py
+++ alpine-objects/src/alpineobjects.py
@@ -1,2 +1,6 @@
-from js import console
+from js import console, document, Event
 console.log("PyScript loaded")
+
+pyscriptReady = Event.new('pyscriptReady')
+
+document.dispatchEvent(pyscriptReady);
